7656bf4731c63a6ebc71584ba20806babc06d9b4
Arthur Mendes Bezerra <arthur.mendes17052004@gmail.com>
2026-04-08 23:09:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+    Variáveis - pedacinho de memoria que eu posso guardar o que eu queiser
+    Algoritmo - receita de bolo - passo a passo para chegar no resultado final
+    logica de programação - fazer o bolo - entender o passo a passo para chegar no resultado final
+
+
+*/
+
+let nome = "Jack-O-Lantern"; //string - texto
+let sobrenome = "Halloween"; //string - texto
+
+
+alert(nome);
+
+console.log(nome);
+console.log(sobrenome);
+
+
